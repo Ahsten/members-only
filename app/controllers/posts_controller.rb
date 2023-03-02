84c15e47e2b186bd8284f44de2a87125584b438e
Luxe17 <hanson1717@live.com>
2023-03-02 14:37:02 -0700

    Added params and create acitons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,19 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    def create
+        @post = Post.new(post_params)
+        @post.user = current_user
+
+        if @post.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
